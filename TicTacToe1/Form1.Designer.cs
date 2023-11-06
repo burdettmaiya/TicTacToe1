8e6f2eabee66757489f882e013fbd25ced5dfee6
MAIYA BURDETT <MAIYABURDETT@exe-coll.ac.uk>
2023-11-06 14:44:03 +0000

    oj

--- TicTacToe1/Form1.Designer.cs
+++ TicTacToe1/Form1.Designer.cs
@@ -43,6 +43,10 @@
             button9 = new Button();
             CPUwatch = new System.Windows.Forms.Timer(components);
             tryagain = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // process1
@@ -58,26 +62,25 @@
             // 
             // label1
             // 
-            label1.AutoSize = true;
             label1.Font = new Font("Broadway", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.Navy;
-            label1.Location = new Point(12, 9);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(32, 84);
             label1.Name = "label1";
-            label1.Size = new Size(148, 19);
+            label1.Size = new Size(23, 31);
             label1.TabIndex = 0;
-            label1.Text = "Player O Wins:";
+            label1.Text = "-";
             label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Broadway", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.ForeColor = Color.DeepPink;
-            label2.Location = new Point(302, 9);
+            label2.ForeColor = Color.Fuchsia;
+            label2.Location = new Point(427, 81);
             label2.Name = "label2";
-            label2.Size = new Size(123, 19);
+            label2.Size = new Size(16, 19);
             label2.TabIndex = 1;
-            label2.Text = "CPU X Wins:";
+            label2.Text = "-";
             label2.Click += label2_Click;
             // 
             // button1
@@ -86,9 +89,9 @@
             button1.BackgroundImageLayout = ImageLayout.Stretch;
             button1.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = Color.DeepPink;
-            button1.Location = new Point(71, 111);
+            button1.Location = new Point(70, 84);
             button1.Name = "button1";
-            button1.Size = new Size(112, 107);
+            button1.Size = new Size(115, 114);
             button1.TabIndex = 2;
             button1.UseVisualStyleBackColor = false;
             button1.Click += PlayerClickButton;
@@ -99,9 +102,9 @@
             button2.BackgroundImageLayout = ImageLayout.Stretch;
             button2.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button2.ForeColor = SystemColors.ButtonHighlight;
-            button2.Location = new Point(180, 111);
+            button2.Location = new Point(179, 84);
             button2.Name = "button2";
-            button2.Size = new Size(112, 107);
+            button2.Size = new Size(115, 114);
             button2.TabIndex = 3;
             button2.UseVisualStyleBackColor = false;
             button2.Click += PlayerClickButton;
@@ -112,9 +115,9 @@
             button3.BackgroundImageLayout = ImageLayout.Stretch;
             button3.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button3.ForeColor = SystemColors.ButtonHighlight;
-            button3.Location = new Point(289, 111);
+            button3.Location = new Point(288, 84);
             button3.Name = "button3";
-            button3.Size = new Size(112, 107);
+            button3.Size = new Size(115, 114);
             button3.TabIndex = 4;
             button3.UseVisualStyleBackColor = false;
             button3.Click += PlayerClickButton;
@@ -125,9 +128,9 @@
             button4.BackgroundImageLayout = ImageLayout.Stretch;
             button4.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button4.ForeColor = SystemColors.ButtonHighlight;
-            button4.Location = new Point(289, 211);
+            button4.Location = new Point(288, 184);
             button4.Name = "button4";
-            button4.Size = new Size(112, 107);
+            button4.Size = new Size(115, 114);
             button4.TabIndex = 7;
             button4.UseVisualStyleBackColor = false;
             button4.Click += PlayerClickButton;
@@ -138,9 +141,9 @@
             button5.BackgroundImageLayout = ImageLayout.Stretch;
             button5.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button5.ForeColor = SystemColors.ButtonHighlight;
-            button5.Location = new Point(180, 211);
+            button5.Location = new Point(179, 184);
             button5.Name = "button5";
-            button5.Size = new Size(112, 107);
+            button5.Size = new Size(115, 114);
             button5.TabIndex = 6;
             button5.UseVisualStyleBackColor = false;
             button5.Click += PlayerClickButton;
@@ -151,9 +154,9 @@
             button6.BackgroundImageLayout = ImageLayout.Stretch;
             button6.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button6.ForeColor = SystemColors.ButtonHighlight;
-            button6.Location = new Point(71, 211);
+            button6.Location = new Point(70, 184);
             button6.Name = "button6";
-            button6.Size = new Size(112, 107);
+            button6.Size = new Size(115, 114);
             button6.TabIndex = 5;
             button6.UseVisualStyleBackColor = false;
             button6.Click += PlayerClickButton;
@@ -164,9 +167,9 @@
             button7.BackgroundImageLayout = ImageLayout.Stretch;
             button7.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button7.ForeColor = SystemColors.ButtonHighlight;
-            button7.Location = new Point(289, 315);
+            button7.Location = new Point(288, 288);
             button7.Name = "button7";
-            button7.Size = new Size(112, 107);
+            button7.Size = new Size(115, 114);
             button7.TabIndex = 10;
             button7.UseVisualStyleBackColor = false;
             button7.Click += PlayerClickButton;
@@ -177,9 +180,9 @@
             button8.BackgroundImageLayout = ImageLayout.Stretch;
             button8.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button8.ForeColor = Color.White;
-            button8.Location = new Point(180, 315);
+            button8.Location = new Point(179, 288);
             button8.Name = "button8";
-            button8.Size = new Size(112, 107);
+            button8.Size = new Size(115, 114);
             button8.TabIndex = 9;
             button8.UseVisualStyleBackColor = false;
             button8.Click += PlayerClickButton;
@@ -190,9 +193,9 @@
             button9.BackgroundImageLayout = ImageLayout.Stretch;
             button9.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             button9.ForeColor = SystemColors.ButtonHighlight;
-            button9.Location = new Point(71, 315);
+            button9.Location = new Point(70, 288);
             button9.Name = "button9";
-            button9.Size = new Size(112, 107);
+            button9.Size = new Size(115, 114);
             button9.TabIndex = 8;
             button9.UseVisualStyleBackColor = false;
             button9.Click += PlayerClickButton;
@@ -205,13 +208,35 @@
             // tryagain
             // 
             tryagain.BackColor = Color.Transparent;
-            tryagain.Location = new Point(143, 50);
+            tryagain.Location = new Point(150, 413);
             tryagain.Name = "tryagain";
-            tryagain.Size = new Size(174, 55);
+            tryagain.Size = new Size(176, 36);
             tryagain.TabIndex = 11;
             tryagain.Text = "Try Again";
             tryagain.UseVisualStyleBackColor = false;
             tryagain.Click += RestartGame;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.O;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(12, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(71, 69);
+            pictureBox1.TabIndex = 12;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = Properties.Resources.X;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(401, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(71, 66);
+            pictureBox2.TabIndex = 13;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // Form1
             // 
@@ -219,6 +244,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(192, 192, 255);
             ClientSize = new Size(484, 461);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Controls.Add(tryagain);
             Controls.Add(button7);
             Controls.Add(button8);
@@ -234,6 +261,8 @@
             Name = "Form1";
             Text = "THE BEST TIC TAC TOE";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -255,5 +284,7 @@
         private Button button1;
         private System.Windows.Forms.Timer CPUwatch;
         private Button tryagain;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
     }
 }
--- TicTacToe1/Form1.cs
+++ TicTacToe1/Form1.cs
@@ -1,33 +1,37 @@
-using System;
+﻿using System; 
 using System.Reflection;
+using System.Text;
 
 namespace TicTacToe1
+    //The title/ name of the code & game. 
 {
     public partial class Form1 : Form
     {
-
-
-
-
-
-        public enum Player
+        //This states if the user can change the code when the game is in process and changing any default code. 
+        
+        private enum Player
         {
-            X, O
+            X, O //These are the constants characters for the game. The protagonist is the 'X' and the 'O' is the antagonist. 
+
         }
 
         Player currentPlayer;
-        Random random = new Random();
+        Random random = new Random(); // The "random" makes sure the player gets the deserved points throughout the game rather than having it go up
+                                      // where it pleases.
         int playerWinCount = 0;
-        int CPUWinCount = 0;
-        List<Button> buttons;
+        int CPUWinCount = 0; // Points start at the bottom "0".
+        List<Button> buttons; //This goes connects to the buttons listed below 
+                              //This goes connects to the buttons listed below "buttons = new List<Button> { button1, button2, button3, button4, button5,
+                              //button6, button7, button8, button9" to create an estimated point system."
+                              
 
-
+        //The code above allows the user to see the points changing when one player scores a point. 
 
         public Form1()
         {
             InitializeComponent();
             RestartGame();
-        }
+        } // Form1 is a class that can be effected by the user, making sure the user can restart the game when requested however (); it is further coded below.
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -46,21 +50,22 @@ namespace TicTacToe1
 
         private void CPUmove(object sender, EventArgs e)
         {
-            if (buttons.Count > 0)
+            if (buttons.Count > 0) // any code in this class will only affect the buttons above 0 so anything from 1-9. (the onlyh buttons activated as false).
             {
-                int index = random.Next(buttons.Count);
-                buttons[index].Enabled = false;
-                currentPlayer = Player.X;
-                buttons[index].Text = currentPlayer.ToString();
-               
-                buttons[index].ForeColor = Color.Transparent;
-                buttons[index].BackgroundImage = TicTacToe1.Properties.Resources.X;
-                buttons.RemoveAt(index);
-                CheckGame();
-                CPUwatch.Stop();
-               
-            }
-        }
+                int index = random.Next(buttons.Count); //This is the command to count up on non-negative numbers to a random class that wins a round. 
+                buttons[index].Enabled = false;         //To allow the user to press the button to activate the next move. In other terms, the button works !
+                currentPlayer = Player.X;               //For this 'if' class it belongs to player x to make sure each character knows what it is supposed to be doing.
+                buttons[index].Text = currentPlayer.ToString(); //When restarting the game, everything goes back to the original state, including the points
+                                                                //except for the board that is showed at the top of the game. 
+                buttons[index].BackColor = Color.Purple;//This colors the character x during the game and throughout.
+
+                buttons.RemoveAt(index); //When restarting the game, the character x will remove itself and start fresh - Except for the point board.
+                CheckGame();             //Activating the code above and making sure everything moves to how it has been designed to do, restart and place the characters.
+                CPUwatch.Stop();         //Stopping the CPUwatch allows the character to stop activating buttons when the game and the player O has finished selecting.
+
+            } //All the code in the class of CPUmove is all effecting the characters and buttons when it is the antagonist's turn to make a move
+        }     
+        
 
         private void PlayerClickButton(object sender, EventArgs e)
         {
@@ -68,20 +73,16 @@ namespace TicTacToe1
 
             currentPlayer = Player.O;
             button.Text = currentPlayer.ToString();
-            
+
             button.Enabled = false;
-            button.BackgroundImage = TicTacToe1.Properties.Resources.O;
+            button.BackColor = Color.LightBlue;
             buttons.Remove(button);
+
             CheckGame();
             CPUwatch.Start();
-            
-        }
+        } //Everything in the PlayerClickButton class is the exact same code for the CPU. 
 
-
-
-
-
-        private void CheckGame()
+                private void CheckGame()
         {
 
             if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
@@ -90,20 +91,20 @@ namespace TicTacToe1
                 || button1.Text == "O" && button6.Text == "O" && button9.Text == "O"
                 || button2.Text == "O" && button5.Text == "O" && button8.Text == "O"
                 || button3.Text == "O" && button5.Text == "O" && button9.Text == "O"
-                || button1.Text == "O" && button5.Text == "O" && button7.Text == "O"
-                || button3.Text == "O" && button4.Text == "O" && button7.Text == "O"
+                || button1.Text == "O" && button5.Text == "O" && button7.Text == "O" //This code here allows the studio to know what makes the player O win the game.
+                || button3.Text == "O" && button4.Text == "O" && button7.Text == "O" //Otherwise the player will either always or never win.
                 )
             {
                 CPUwatch.Stop();
-                MessageBox.Show("PlayerWins", "Boss Says");
+                MessageBox.Show("YOU MADE IT SOLDIER", "Commander Comments"); //If the player O wins then they will receive this comment on the left.
                 playerWinCount++;
-                label1.Text = "Player Wins : " + playerWinCount;
-                RestartGame();
+                label1.Text = "" + playerWinCount; //This recognises the count update and will change the scoreboard and add 1 point per win.
+                RestartGame(); //Makes the game start from the beginning except for the point system 
             }
             else if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
                 || button6.Text == "X" && button5.Text == "X" && button4.Text == "X"
                 || button7.Text == "X" && button8.Text == "X" && button9.Text == "X"
-                || button1.Text == "X" && button6.Text == "X" && button9.Text == "X" 
+                || button1.Text == "X" && button6.Text == "X" && button9.Text == "X" //Exactly the same as above.
                 || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
                 || button3.Text == "X" && button5.Text == "X" && button9.Text == "X"
                 || button1.Text == "X" && button5.Text == "X" && button7.Text == "X"
@@ -111,9 +112,9 @@ namespace TicTacToe1
                 )
             {
                 CPUwatch.Stop();
-                MessageBox.Show("CPU Wins", "Boss Says");
+                MessageBox.Show("...", "Commander Left"); //This comment will be received to the player if they lose a round.
                 CPUWinCount++;
-                label2.Text = "CPU Wins: " + CPUWinCount;
+                label2.Text = "" + CPUWinCount;
                 RestartGame();
             }
         }
@@ -124,22 +125,30 @@ namespace TicTacToe1
         private void RestartGame()
         {
             buttons = new List<Button> { button1, button2, button3, button4, button5,
-                button6, button7, button8, button9 };
+                button6, button7, button8, button9 }; //Buttons from 1-9 are the only buttons to be used in game for the characters to take place.
 
-            foreach (Button x in buttons)
+            foreach (Button x in buttons) //this means every button in the perimeters of 1-9
             {
-                x.Enabled = true;
+                x.Enabled = true; //This allows the player to SELECT a button 
 
-                x.ResetText();
+                x.Text = ""; //Nothing will be displayed in each button when a character has not selected a button.
+
+                x.BackColor = DefaultBackColor;  //The colour is similar to white. A basic background to identify the lining of each square and not
+                                                 // to confuse the player where the buttons are placed for those who are colour blinded.
                 
-                x.ForeColor = Color.Transparent;
-
             }
 
         }
 
+        private void pictureBox1_Click(object sender, EventArgs e) // this is the player O's profile picture
+        {
 
+        }
 
+        private void pictureBox2_Click(object sender, EventArgs e) // this is the player X's profile picture
+        {
+
+        }
     }
 
 
